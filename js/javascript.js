8fc3c05fbad58782b5805bd7e23c1c81d6f997c0
Emmawilliams16 <157321508+Emmawilliams16@users.noreply.github.com>
2024-04-18 15:26:17 -0400

    additions

--- js/javascript.js
+++ js/javascript.js
@@ -11,3 +11,28 @@ function signUp() {
     alert("Thank you for signing up for our emails!");
     window.close();
 }
+
+
+
+
+
+
+let mobileMenuOpen = false;
+
+$("#mobile-menu-button").click(function(){
+    if(mobileMenuOpen == false) {
+    //if mobile menu is closed, open it
+   
+        $("#main-menu").removeClass("display-none-mobile");
+        mobileMenuOpen = true;
+        $("#mobile-menu-text").text("Close");
+        $("#mobile-menu-icon").attr("src", "assets/images/close.svg");
+    }
+    //if mobile menu is open, close it
+    else {
+        $("#main-menu").addClass("display-none-mobile");
+        mobileMenuOpen = false;
+        $("#mobile-menu-text").text("Menu");
+        $("#mobile-menu-icon").attr("src", "assets/images/menu.svg");
+    }
+});
